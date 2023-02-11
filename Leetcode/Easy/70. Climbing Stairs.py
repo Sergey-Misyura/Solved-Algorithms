bbb134e7a4f9aca023cb5e41129657b5b9363fc4
Sergey Misyura <101212758+Sergey-Misyura@users.noreply.github.com>
2023-02-11 11:32:52 +0300

    Added leetcode 70

---
+++ Leetcode/Easy/70. Climbing Stairs.py
@@ -0,0 +1,20 @@
+"""
+70. Climbing Stairs
+(Easy complexity)
+
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+"""
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        if n < 2: return n
+
+        dp = [0]*(n+1)
+        dp[1], dp[2] = 1, 2
+
+        for i in range(3, n+1):
+            dp[i] = dp[i-1]+ dp[i-2]
+        return dp[n]
