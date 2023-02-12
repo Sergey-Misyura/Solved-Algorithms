068bf24dd5eb760c814b8f8adeae03e8806c9cff
Sergey Misyura <101212758+Sergey-Misyura@users.noreply.github.com>
2023-02-12 21:40:56 +0300

    Added leetcode 27

---
+++ Leetcode/Easy/27. Remove Element.py
@@ -0,0 +1,25 @@
+"""
+27. Remove Element
+(Easy complexity)
+
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. 
+The relative order of the elements may be changed.
+
+Since it is impossible to change the length of the array in some languages, 
+you must instead have the result be placed in the first part of the array nums. 
+More formally, if there are k elements after removing the duplicates, then the first k elements of nums should 
+hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+"""
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        count = 0
+        for i in range(len(nums)):
+            if nums[i]!=val:
+                nums[count] = nums[i]
+                count+=1
+        return count
