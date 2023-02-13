b3f422a725a1d875ba241b2a403af11915af747c
Sergey Misyura <101212758+Sergey-Misyura@users.noreply.github.com>
2023-02-13 21:50:59 +0300

    Added leetcode 2235

---
+++ Leetcode/Easy/2235. Add Two Integers.py
@@ -0,0 +1,10 @@
+"""
+2235. Add Two Integers
+(Easy complexity)
+
+Given two integers num1 and num2, return the sum of the two integers.
+"""
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return sum([num1,num2])
