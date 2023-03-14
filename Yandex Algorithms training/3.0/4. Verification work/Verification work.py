a380449c01267c6327104d9893df8f53fcaef2a2
Sergey Misyura <101212758+Sergey-Misyura@users.noreply.github.com>
2023-03-14 23:45:16 +0300

    Update Yandex 3.0 4-solution

--- Yandex Algorithms training/3.0/4. Verification work/Verification work.py
+++ Yandex Algorithms training/3.0/4. Verification work/Verification work.py
@@ -4,26 +4,27 @@ row = int(input())
 seat = int(input())
 
 result_list = []
-taken_seat_num = (row - 1) * 2 + seat
+taken_seat_num  = (row-1)*2+seat
 
 if taken_seat_num - K <= 0 and taken_seat_num + K > N:
     print(-1)
-
+   
 if taken_seat_num - K > 0:
     prev_seat_num = taken_seat_num - K
-    prev_row = (prev_seat_num + 1) // 2
-    prev_seat = prev_seat_num - (prev_row - 1) * 2
+    prev_row = (prev_seat_num+1)//2
+    prev_seat = prev_seat_num - (prev_row-1)*2
     result_list.append([prev_row, prev_seat])
+   
 
 if taken_seat_num + K <= N:
     after_seat_num = taken_seat_num + K
-    after_row = (after_seat_num + 1) // 2
-    after_seat = after_seat_num - (after_row - 1) * 2
+    after_row = (after_seat_num+1)//2
+    after_seat = after_seat_num - (after_row-1)*2
     result_list.append([after_row, after_seat])
 
-if len(result_list) == 1:
+if len(result_list)==1:
     print(result_list[0][0], result_list[0][1])
-elif len(result_list) == 2:
+elif len(result_list)==2:
     if row - result_list[0][0] >= result_list[1][0] - row:
         print(result_list[1][0], result_list[1][1])
     else:
