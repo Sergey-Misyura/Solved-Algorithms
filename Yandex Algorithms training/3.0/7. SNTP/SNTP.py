57da220129991dd6bac8922bd60608f610bf5d4a
Sergey Misyura <101212758+Sergey-Misyura@users.noreply.github.com>
2023-03-14 23:46:09 +0300

    Update Yandex 3.0 7-solution

--- Yandex Algorithms training/3.0/7. SNTP/SNTP.py
+++ Yandex Algorithms training/3.0/7. SNTP/SNTP.py
@@ -2,22 +2,23 @@ time_A = input()
 time_B = input()
 time_C = input()
 
-
 def time_in_sec(time):
-    h, m, s = map(int, time.split(':'))
-    return (h * 60 + m) * 60 + s
+    h,m,s = map(int, time.split(':'))
+    return (h*60+m)*60+s
 
-
-time_A = time_in_sec(time_A)
-time_B = time_in_sec(time_B)
-time_C = time_in_sec(time_C)
-delay = (time_C - time_A) / 2
-delay = int(delay) + 1 if delay - int(delay) == 0.5 else round(delay)
-output_time = time_B + delay
+def time_to_str(time):
+	time = str(time) if time > 9 else '0'+str(time)
+	return time
+   
+time_A, time_C = time_in_sec(time_A), time_in_sec(time_C)
+time_C += 24*3600 if time_A > time_C else 0
+delay = (time_C - time_A)/2
+delay = int(delay)+1 if delay - int(delay) == 0.5 else round(delay)
+output_time = time_in_sec(time_B) + delay
 
 h = output_time // 3600
-m = (output_time - h * 3600) // 60
-s = (output_time - h * 3600) - m * 60
+m = (output_time - h*3600)//60
+s = (output_time - h*3600) - m*60
 h = h % 24
-
-print(f'{h}:{m}:{s}')
+output = time_to_str(h)+':'+time_to_str(m)+':'+time_to_str(s)
+print(output)
