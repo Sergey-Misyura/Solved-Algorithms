a6c465fa648fc8f9043aaeeb7319b7af12c42fb3
Sergey Misyura <101212758+Sergey-Misyura@users.noreply.github.com>
2023-03-14 23:43:30 +0300

    Update Yandex 3.0 1-solution

--- Yandex Algorithms training/3.0/1. Hystogram/Hystogram.py
+++ Yandex Algorithms training/3.0/1. Hystogram/Hystogram.py
@@ -1,21 +1,23 @@
-s = input()
+with open('input.txt', 'r') as f:
+    s = f.read()
 
 freq_dict, max_freq = {}, 0
 
-for sym in s:
-    freq_dict[sym] = freq_dict.setdefault(sym, 0) + 1
+for idx in range(len(s)):
+    if s[idx]!=' ' and s[idx]!='\n':
+        freq_dict[s[idx]] = freq_dict.setdefault(s[idx], 0) + 1
 
 max_freq = max(freq_dict.values())
 sorted_sym = sorted(freq_dict.keys())
 
 for row_weight in range(max_freq, 0, -1):
 
-    row = []
+    row=[]
     for sym in sorted_sym:
         if freq_dict[sym] >= row_weight:
             row.append('#')
         else:
             row.append(' ')
     print("".join(row))
-
+   
 print("".join(sorted_sym))
