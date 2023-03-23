d3ff0219a9a621ca62525e6060424e1ef16d0fbf
Sergey-Misyura <101212758+Sergey-Misyura@users.noreply.github.com>
2023-03-23 21:42:51 +0300

    Added Leetcode 54, 202, 1706

---
+++ Leetcode/Medium/54. Spiral Matrix.py
@@ -0,0 +1,10 @@
+"""
+54. Spiral Matrix
+(Easy complexity)
+
+Given an m x n matrix, return all elements of the matrix in spiral order.
+"""
+
+class Solution:
+    def spiralOrder(self, matrix):
+        return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
