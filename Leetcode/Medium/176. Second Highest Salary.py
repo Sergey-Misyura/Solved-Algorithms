54c10fa4af8c36a04848223191afa0c6784d55c4
Sergey-Misyura <101212758+Sergey-Misyura@users.noreply.github.com>
2023-04-02 21:32:22 +0300

    Added Leetcode 176, 608

---
+++ Leetcode/Medium/176. Second Highest Salary.py
@@ -0,0 +1,24 @@
+"""
+176. Second Highest Salary
+(Medium complexity)
+
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key column for this table.
+Each row of this table contains information about the salary of an employee.
+
+
+Write an SQL query to report the second highest salary from the Employee table. If there is no second highest salary, the query should report null.
+
+The query result format is in the following example.
+"""
+
+# Write your MySQL query statement below
+SELECT MAX(Salary) as SecondHighestSalary FROM Employee
+WHERE Salary < (SELECT max(Salary) FROM Employee)
