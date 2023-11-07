dcbd7faad46837a5d46193eae97fe0bf958282e1
Sergey-Misyura <101212758+Sergey-Misyura@users.noreply.github.com>
2023-11-07 21:14:26 +0300

    Added Leetcode 1845

---
+++ Leetcode/Medium/1845. Seat Reservation Manager.py
@@ -0,0 +1,29 @@
+"""
+1845. Seat Reservation Manager
+(Medium complexity)
+
+Design a system that manages the reservation state of n seats that are numbered from 1 to n.
+
+Implement the SeatManager class:
+
+SeatManager(int n) Initializes a SeatManager object that will manage n seats numbered from 1 to n. All seats are initially available.
+int reserve() Fetches the smallest-numbered unreserved seat, reserves it, and returns its number.
+void unreserve(int seatNumber) Unreserves the seat with the given seatNumber.
+"""
+
+
+class SeatManager:
+
+    def __init__(self, n: int):
+
+
+    def reserve(self) -> int:
+
+
+    def unreserve(self, seatNumber: int) -> None:
+
+
+# Your SeatManager object will be instantiated and called as such:
+# obj = SeatManager(n)
+# param_1 = obj.reserve()
+# obj.unreserve(seatNumber)
