b43c9a2494e029030f2b869f490c3bdd69fd6168
Sergey-Misyura <101212758+Sergey-Misyura@users.noreply.github.com>
2023-11-17 22:58:14 +0300

    Added Leetcode 1877

---
+++ Leetcode/Medium/1877. Minimize Maximum Pair Sum in Array.py
@@ -0,0 +1,26 @@
+"""
+1877. Minimize Maximum Pair Sum in Array
+(Medium complexity)
+
+The pair sum of a pair (a,b) is equal to a + b. The maximum pair sum is the largest pair sum in a list of pairs.
+
+For example, if we have pairs (1,5), (2,3), and (4,4), the maximum pair sum would be max(1+5, 2+3, 4+4) = max(6, 5, 8) = 8.
+Given an array nums of even length n, pair up the elements of nums into n / 2 pairs such that:
+
+Each element of nums is in exactly one pair, and
+The maximum pair sum is minimized.
+Return the minimized maximum pair sum after optimally pairing up the elements.
+"""
+
+
+class Solution:
+    def minPairSum(self, nums: List[int]) -> int:
+        nums.sort()
+        max_sum = 0
+        l, r = 0, len(nums) - 1
+
+        while l < r:
+            max_sum = max(nums[l] + nums[r], max_sum)
+            l, r = l + 1, r - 1
+
+        return max_sum
