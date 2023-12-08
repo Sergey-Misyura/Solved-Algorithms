6fd04da68fbdb8e030cd5d21f5a20d0e04f0caa7
Sergey-Misyura <101212758+Sergey-Misyura@users.noreply.github.com>
2023-12-08 12:35:42 +0300

    Update Leetcode 1424

--- Leetcode/Medium/1424. Diagonal Traverse II.py
+++ Leetcode/Medium/1424. Diagonal Traverse II.py
@@ -7,10 +7,11 @@ Given a 2D integer array nums, return all elements of nums in diagonal order as 
 
 class Solution:
     def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
-        ans = []
-        for i, r in enumerate(nums):
-            for j, a in enumerate(r):
-                if len(ans) <= i + j:
-                    ans.append([])
-                ans[i + j].append(a)
-        return [a for r in res for a in reversed(r)]
+        diags_len = max(max([len(row) for row in nums]), len(nums))
+        diags = [[] for _ in range(2*diags_len-1)]
+
+        for n_row, row in enumerate(nums):
+            for n_col, num in enumerate(row):
+                diags[n_row+n_col].append(num)
+        print(diags)
+        return [num for diag in diags for num in reversed(diag)]
