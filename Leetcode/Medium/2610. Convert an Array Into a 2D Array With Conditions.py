36b8aeef89bc091c8ccab6289ff21aac372d723f
Sergey-Misyura <101212758+Sergey-Misyura@users.noreply.github.com>
2024-01-02 15:09:36 +0300

    Added Leetcode 2610

---
+++ Leetcode/Medium/2610. Convert an Array Into a 2D Array With Conditions.py
@@ -0,0 +1,28 @@
+"""
+2610. Convert an Array Into a 2D Array With Conditions
+(Medium complexity)
+
+You are given an integer array nums. You need to create a 2D array from nums satisfying the following conditions:
+
+The 2D array should contain only the elements of the array nums.
+Each row in the 2D array contains distinct integers.
+The number of rows in the 2D array should be minimal.
+Return the resulting array. If there are multiple answers, return any of them.
+
+Note that the 2D array can have a different number of elements on each row.
+"""
+
+
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        freq = [0] * (len(nums) + 1)
+        ans = []
+
+        for num in nums:
+            if freq[num] >= len(ans):
+                ans.append([])
+
+            ans[freq[num]].append(num)
+            freq[num] += 1
+
+        return ans
