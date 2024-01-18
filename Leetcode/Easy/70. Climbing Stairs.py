83d0618f23fb733f9e461db81750023d8bb7692d
Sergey-Misyura <101212758+Sergey-Misyura@users.noreply.github.com>
2024-01-18 10:38:45 +0300

    Update Leetcode 70

--- Leetcode/Easy/70. Climbing Stairs.py
+++ Leetcode/Easy/70. Climbing Stairs.py
@@ -9,12 +9,7 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
 
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
-        if n < 2: return n
-
-        dp = [0]*(n+1)
-        dp[1], dp[2] = 1, 2
-
-        for i in range(3, n+1):
-            dp[i] = dp[i-1]+ dp[i-2]
-        return dp[n]
+        prev = cur = 1
+        for _ in range(n-1):
+            cur, prev = cur + prev, cur
+        return cur
