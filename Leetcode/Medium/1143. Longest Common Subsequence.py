7341e98e7ebcb5ec430a7fee00895db42a2d160b
Sergey-Misyura <101212758+Sergey-Misyura@users.noreply.github.com>
2024-01-25 09:42:46 +0300

    Added Leetcode 1143

---
+++ Leetcode/Medium/1143. Longest Common Subsequence.py
@@ -0,0 +1,25 @@
+"""
+1143. Longest Common Subsequence
+(Medium complexity)
+
+Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+For example, "ace" is a subsequence of "abcde".
+A common subsequence of two strings is a subsequence that is common to both strings."""
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n, m = len(text1), len(text2)
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+        for row in range(1, n + 1):
+            for col in range(1, m + 1):
+                if text1[row - 1] == text2[col - 1]:
+                    dp[row][col] = dp[row - 1][col - 1] + 1
+                else:
+                    dp[row][col] = max(dp[row - 1][col], dp[row][col - 1])
+
+        return dp[n][m]
