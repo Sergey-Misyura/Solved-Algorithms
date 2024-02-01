c40192bce07a64ea7333713d4dd5125f01a25fb0
Sergey-Misyura <101212758+Sergey-Misyura@users.noreply.github.com>
2024-02-01 22:02:38 +0300

    Update Leetcode 189

--- Leetcode/Medium/189. Rotate Array.py
+++ Leetcode/Medium/189. Rotate Array.py
@@ -10,17 +10,6 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k %= len(nums)
-        nums.reverse()
-        
-        l_num, r_num = 0, k-1
-        while l_num < r_num:
-            nums[l_num], nums[r_num] = nums[r_num], nums[l_num]
-            l_num += 1
-            r_num -= 1
-            
-        l_num, r_num = k, len(nums) - 1
-        while l_num < r_num:
-            nums[l_num], nums[r_num] = nums[r_num], nums[l_num]
-            l_num += 1
-            r_num -= 1
+        n = len(nums)
+        k = k % n
+        nums[:] = nums[n - k: n] + nums[: n - k]
