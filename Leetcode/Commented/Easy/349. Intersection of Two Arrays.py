cb65293d8df38c42fbd0b57f44b31346a5206837
Sergey-Misyura <101212758+Sergey-Misyura@users.noreply.github.com>
2024-03-10 21:12:46 +0300

    Added Leetcode 349

---
+++ Leetcode/Commented/Easy/349. Intersection of Two Arrays.py
@@ -0,0 +1,12 @@
+"""
+349. Intersection of Two Arrays
+(Easy complexity)
+
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+"""
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # ответ - пересечение множеств
+        return set(nums1).intersection(set(nums2))
